eb347525368409703683d19135fb4d5dfd93c757
Danita <roboticmisfit@gmail.com>
2018-07-11 16:07:50 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Write a Java program to print a box, an oval, or a diamond (choose one) using asterisks.
+
+        System.out.println("BOX");
+        System.out.println("* * * * * * *");
+        System.out.println("*\t \t \t*");
+        System.out.println("*\t \t \t*");
+        System.out.println("*\t \t \t*");
+        System.out.println("*\t \t \t*");
+        System.out.println("* * * * * * *");
+
+        //Create new line
+        System.out.println("\n");
+
+
+        System.out.println("OVAL");
+        System.out.println("\t* * *\t");
+        System.out.println("  *\t\t  *");
+        System.out.println(" *\t\t   *");
+        System.out.println("  *\t\t  *");
+        System.out.println("\t* * *\t");
+
+        //Create new line
+        System.out.println("\n");
+
+        System.out.println("DIAMONDS");
+        System.out.println("\t\t*\t");
+        System.out.println("\t*\t\t*");
+        System.out.println("*\t\t\t\t*");
+        System.out.println("\t*\t\t*");
+        System.out.println("\t\t*\t");
+
+        //Create new line
+        System.out.println("\n");
+        System.out.println("\t\t*\t\t");
+        System.out.println("\t *\t   *");
+        System.out.println("   *\t\t *");
+        System.out.println("\t *\t   *");
+        System.out.println("\t\t*\t\t");
+
+
+
+
+
+    }
+
+}
